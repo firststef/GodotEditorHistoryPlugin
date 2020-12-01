e4cd51d9e4d6e09e9d7041e79d5a575b94ef368f
firststef <firstbossspike@gmail.com>
2020-12-02 01:57:30 +0200

    Updated functions to follow the new api - adding params

--- addons/editor_history/plugin.gd
+++ addons/editor_history/plugin.gd
@@ -10,22 +10,23 @@ func _enter_tree():
 	dock = dock_scene.instance()
 	add_control_to_dock(DOCK_SLOT_LEFT_UL, dock)
 	ea = get_editor_interface().get_editor_actions()
-	ea.add_on_action_executing("SceneTreeEditor/nothing_selected", Callable(self, "on_start_selected_changed"))
-	ea.add_on_action_executed("SceneTreeEditor/nothing_selected", Callable(self, "on_end_selected_changed"))
+	ea.add_on_action_executing("SceneTreeEditor/nothing_selected", Callable(self, "on_start_selected_changed"), ["SceneTreeEditor/nothing_selected"])
+	ea.add_on_action_executed("SceneTreeEditor/nothing_selected", Callable(self, "on_end_selected_changed"), ["SceneTreeEditor/nothing_selected"])
 
 func _exit_tree():
-	print("Exiting EditorHistory plugin ", dock)
 	remove_control_from_docks(dock)
 	dock.queue_free()
 	ea.clear_execute_cb()
 
-func on_start_selected_changed():
-	pass
+func on_start_selected_changed(action_name: String):
+	print("executing " + action_name)
 
-func on_end_selected_changed():
+func on_end_selected_changed(action_name: String):
 	var item = item_scene.instance()
-	item.get_node("HBoxContainer/Label").text = "nothing_selected"
-	item.full_action_name = "SceneTreeEditor/nothing_selected"
+	var splits = action_name.split("/")
+	var text_n = splits[1] if (splits.size() == 2) else action_name
+	item.get_node("HBoxContainer/Label").text = text_n
+	item.full_action_name = action_name
 	item.connect("re_action", Callable(self, "_re_action"))
 	dock.get_node("ScrollContainer/VBoxContainer").add_child(item)
 
